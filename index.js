82abfea0c46bbde70f70baee8fc70c10dee6fbcf
Decidoro <gomezdecidoro98@gmail.com>
2023-04-18 21:33:17 -0400

    message update

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const mongoose = require('mongoose');
 const Inventory = require('./models/product');
 
 mongoose.set("strictQuery", false);
-
+//this code connects the application to a data server
 mongoose.connect('mongodb://localhost:27017/sportApp', {UseNewUrlParser: true, useUnifiedTopology:true})
     .then(()=>{
         console.log('SUCCESS, CONNECTED TO SPORTS MONGO!')
